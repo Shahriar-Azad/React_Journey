7afaf8803a5190b080090e4801b2420e9dc3dc6a
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-07 23:44:15 +0600

    more

--- react_journey/src/ThemeToggle.js
+++ react_journey/src/ThemeToggle.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 export default function ThemeToggle() {
   const [darkMode, setDarkMode] = useState(false);
 
-//   const toggleTheme = () => {
-//     setDarkMode(!darkMode);
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
 //     document.body.style.backgroundColor = darkMode ? '#fff' : '#222';
 //     document.body.style.color = darkMode ? '#000' : '#fff';
 //   };
