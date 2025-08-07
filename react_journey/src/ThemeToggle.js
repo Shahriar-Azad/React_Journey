956239c0cb364c32bcf7f60576a0b477e1791350
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-07 23:43:58 +0600

    half done

---
+++ react_journey/src/ThemeToggle.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function ThemeToggle() {
+  const [darkMode, setDarkMode] = useState(false);
+
+//   const toggleTheme = () => {
+//     setDarkMode(!darkMode);
+//     document.body.style.backgroundColor = darkMode ? '#fff' : '#222';
+//     document.body.style.color = darkMode ? '#000' : '#fff';
+//   };
+
+//   return (
+//     <button onClick={toggleTheme}>
+//       {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+//     </button>
+//   );
+}
