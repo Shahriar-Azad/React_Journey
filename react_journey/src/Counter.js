c1bca27715b41e5d97740f3d16b1130ff9f13bc1
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-08 23:41:16 +0600

    added five

--- react_journey/src/Counter.js
+++ react_journey/src/Counter.js
@@ -4,10 +4,10 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div>
-{/* //       <h2>Count: {count}</h2>
-//       <button onClick={() => setCount(count + 1)}> + </button>
-//       <button onClick={() => setCount(count - 1)}> - </button>
-//       <button onClick={() => setCount(0)}> Reset </button> */}
+         <h2>Count: {count}</h2>
+       {/* <button onClick={() => setCount(count + 1)}> + </button>
+       <button onClick={() => setCount(count - 1)}> - </button>
+       <button onClick={() => setCount(0)}> Reset </button> */}
     </div>
   );
 }
