721c1c727e936dd360f67349f19ef8c063e15607
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-08 23:40:23 +0600

    added four

--- react_journey/src/Counter.js
+++ react_journey/src/Counter.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 export default function Counter() {
   const [count, setCount] = useState(0);
   return (
-//     <div>
-//       <h2>Count: {count}</h2>
+    <div>
+{/* //       <h2>Count: {count}</h2>
 //       <button onClick={() => setCount(count + 1)}> + </button>
 //       <button onClick={() => setCount(count - 1)}> - </button>
-//       <button onClick={() => setCount(0)}> Reset </button>
-//     </div>
+//       <button onClick={() => setCount(0)}> Reset </button> */}
+    </div>
   );
 }
