fe48f33af0e18a8a9993a85b639154002ddcfbad
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-08 23:39:35 +0600

    added two

--- react_journey/src/Counter.js
+++ react_journey/src/Counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default function Counter() {
-//   const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 //   return (
 //     <div>
 //       <h2>Count: {count}</h2>
