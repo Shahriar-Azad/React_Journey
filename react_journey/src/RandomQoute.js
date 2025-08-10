3b69469c43d9e73f60bf0a349d831624299ee8c9
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-10 23:43:40 +0600

    four

--- react_journey/src/RandomQoute.js
+++ react_journey/src/RandomQoute.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const quotes = [
   "Stay hungry, stay foolish.",
   "Simplicity is the ultimate sophistication.",
-
+  "Code is like humor. When you have to explain it, it’s bad."
 ];
 
 
