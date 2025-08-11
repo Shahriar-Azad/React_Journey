3199b0943d8157fab97514fcf138983b0a559f1a
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-11 23:38:49 +0600

    added

--- react_journey/src/ToDo.js
+++ react_journey/src/ToDo.js
@@ -4,5 +4,17 @@ export default function TodoList() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
 
 }
