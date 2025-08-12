5f59214b0d43bc662ce4d3149847cdb38b309d94
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-12 23:54:21 +0600

    one

--- react_journey/src/ToDo.js
+++ react_journey/src/ToDo.js
@@ -15,6 +15,18 @@ export default function TodoList() {
     const newTodos = todos.filter((_, i) => i !== index);
     setTodos(newTodos);
   };
-
-
+// ============= Yet to commit this section =============
+//   return (
+//     <div>
+//       <input value={input} onChange={(e) => setInput(e.target.value)} />
+//       <button onClick={addTodo}>Add</button>
+//       <ul>
+//         {todos.map((todo, idx) => (
+//           <li key={idx} onClick={() => removeTodo(idx)}>
+//             {todo}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
 }
