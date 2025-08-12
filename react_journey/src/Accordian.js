1b0cc6f005d808816b302cbc1155ad519c03f0da
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-12 23:55:28 +0600

    four

--- react_journey/src/Accordian.js
+++ react_journey/src/Accordian.js
@@ -4,7 +4,10 @@ export default function Accordion() {
   const [open, setOpen] = useState(false);
   return (
     <div>
-
+      <button onClick={() => setOpen(!open)}>
+    
+      </button>
+    
     </div>
   );
 }
